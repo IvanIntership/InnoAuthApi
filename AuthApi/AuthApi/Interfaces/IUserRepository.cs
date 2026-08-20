e55d7ea36b11560bea9866256c8f209e0a39a8f1
Lapunov Ivan <lapunoujohn@gmail.com>
2026-08-20 11:51:15 +0300

    Add repository (#3)

---
+++ AuthApi/AuthApi/Interfaces/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using AuthApi.API.Entities;
+
+namespace AuthApi.API.Interfaces;
+
+public interface IUserRepository
+{
+    Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task DeleteAsync(User user, CancellationToken cancellationToken = default);
+    Task AddAsync(User user, CancellationToken cancellationToken = default);
+}
---
+++ AuthApi/AuthApi/Repositories/UserRepository.cs
@@ -0,0 +1,33 @@
+﻿using AuthApi.API.Data;
+using AuthApi.API.Entities;
+using AuthApi.API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthApi.API.Repositories;
+
+public sealed class UserRepository : IUserRepository
+{
+    private readonly AuthApiDbContext _dbContext;
+
+    public UserRepository(AuthApiDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public async Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        return await _dbContext.Users.FirstOrDefaultAsync(u => u.Id == id, cancellationToken);
+    }
+
+    public async Task DeleteAsync(User user, CancellationToken cancellationToken = default)
+    {
+        _dbContext.Users.Remove(user);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task AddAsync(User user, CancellationToken cancellationToken = default)
+    {
+        _dbContext.Users.Add(user);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+    }
+}
