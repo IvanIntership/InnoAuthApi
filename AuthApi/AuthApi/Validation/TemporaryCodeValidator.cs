17cb4d27336613fd1e300891e14276d4a1c19971
Lapunov Ivan <lapunoujohn@gmail.com>
2026-08-25 11:52:19 +0300

    Add validation (#5)

---
+++ AuthApi/AuthApi/Validation/TemporaryCodeValidator.cs
@@ -0,0 +1,12 @@
+﻿using AuthApi.API.Dtos;
+using FluentValidation;
+
+namespace AuthApi.API.Validation;
+
+public sealed class TemporaryCodeValidator : AbstractValidator<TemporaryCode>
+{
+    public TemporaryCodeValidator()
+    {
+        RuleFor(x => x.Code).NotEmpty().WithMessage("Code is required");
+    }
+}
